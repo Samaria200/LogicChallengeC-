b1390f515a9991d274cc554bacb5cc9cedeb4f3d
Samaria200 <84305522+Samaria200@users.noreply.github.com>
2023-01-17 11:47:22 +0530

    Add files via upload

---
+++ Q2.cs
@@ -0,0 +1,32 @@
+﻿
+using System; 
+Console.WriteLine("Enter the booking time: ");
+double time = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("Enter the fare: ");
+double fare = Convert.ToDouble(Console.ReadLine());
+if (time > 6 && time < 9)
+{
+    fare = fare + (fare * 0.1);
+    Console.WriteLine(fare);
+}
+else if (time > 9 & time <= 17)
+{
+    fare = fare + (fare * 0.2);
+    Console.WriteLine(fare);
+}
+else if (time > 17 & time <= 23)
+{
+    fare = fare + (fare * 0.07);
+    Console.WriteLine(fare);
+}
+else if (time > 24)
+{
+    Console.WriteLine("Not valid");
+}
+else
+{
+    fare = fare + (fare * 0.05);
+    Console.WriteLine(fare);
+}
+
+
