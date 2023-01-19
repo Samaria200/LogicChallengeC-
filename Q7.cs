de29918f57c4cf236cac5c7938b1cf23f20b0030
Samaria200 <84305522+Samaria200@users.noreply.github.com>
2023-01-19 10:21:07 +0530

    Add files via upload

---
+++ Q7.cs
@@ -0,0 +1,29 @@
+﻿//Accept Passenger name 
+Console.WriteLine("Enter the passenger name : ");
+string? passengerName = Console.ReadLine();
+
+//Accept total miles travelled 
+Console.WriteLine("Enter the total miles travelled : ");
+int milesTravelled = Convert.ToInt32(Console.ReadLine());
+
+
+switch (milesTravelled)
+{
+    case (>= 10000 and < 20000):
+        Console.WriteLine("You are awarded with 10 flyer points!");
+        break;
+    case (>= 20000 and < 30000):
+        Console.WriteLine("You are awarded with 20 flyer points!");
+        break;
+    case (>= 50000 and < 100000):
+        Console.WriteLine("You are awarded with 30 flyer points!");
+        break;
+    case (>= 100000):
+        Console.WriteLine("You are awarded with 50 flyer points!");
+        break;
+    default:
+        Console.WriteLine(+milesTravelled + "is not right!");
+        break;
+
+}
+
